cd0729c603213a8f8d8c6a3e017e796f11598325
Naoya Kishimoto <naoya.kishimoto1015@gmail.com>
2022-10-18 16:24:06 +0900

    Initial Commit

---
+++ sauna watch Watch App/sauna_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  sauna_watchApp.swift
+//  sauna watch Watch App
+//
+//  Created by 岸本直也 on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct sauna_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
